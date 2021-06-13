6365e5e243c2aa4a00fd9659bf585d44d4be483d
user <user@MacBook-Pro-user.local>
2021-06-13 23:41:31 +0300

    fix cache

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,21 +1,16 @@
 const mcache = require('memory-cache');
 
 const cache = (duration) => (req, res, next) => {
-  const key = `__express__${req.params.buildId}`,
+  const key = req.params.buildId,
     cachedBody = mcache.get(key);
-  if (
-    cachedBody ||
-    mcache.keys().indexOf(`__express__${req.params.buildId}`) !== -1
-  ) {
-    res.send(cachedBody);
-    return;
-  }
+  if (mcache.keys().indexOf(req.params.buildId) !== -1) return res.json(cachedBody);
   res.sendResponse = res.send;
   res.send = (body) => {
-    mcache.put(key, body, duration * 1000 * 3600);
+    mcache.put(key, body, duration * 1000);
     res.sendResponse(body);
   };
   next();
+
 };
 
 module.exports = cache;
