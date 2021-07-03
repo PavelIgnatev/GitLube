012f7df1411a6cbda5021a55be9dc9c7f425594a
PavelIgnatev <palllkaignatev@gmail.com>
2021-07-03 13:11:10 +0300

    fix

--- src/utils/cloneRepoByCommitHash.js
+++ src/utils/cloneRepoByCommitHash.js
@@ -38,11 +38,9 @@ module.exports.cloneRepoByCommitHash = async (
     });
 
     await rmdir(repoPath, true);
-    await rmdir(repoPath, true);
 
     return result.stdout + `\n[1;33m${result.stderr}`;
   } catch (error) {
-    await rmdir(repoPath, true);
     await rmdir(repoPath, true);
     throw error.stderr;
   }
