2c5d7d4dbb714d767939a6126a2c0ca7837f4049
PavelIgnatev <palllkaignatev@gmail.com>
2021-07-07 04:51:21 +0300

    fix react

--- client/src/router/PageRouter.jsx
+++ client/src/router/PageRouter.jsx
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import BuildDetaildsPage from '../components/pages/BuildDetailsPage/BuildDetailsPage.jsx';
 import BuildsHistoryPage from '../components/pages/BuildHistoryPage/BuildHistoryPage.jsx';
 import SettingsPage from '../components/pages/SettingsPage/SettingsPage.jsx';
@@ -12,21 +12,29 @@ const RepoHeader = () => {
   const getter = settings.getterSettings;
   return (
     <div className="app-page">
-      {(builds.BuildInfoError || builds.buildLogError || builds.buildListError || settings.settingsErorr) && (
-        <Redirect to="/page-not-found"></Redirect>
-      )}
-      {settings.status === "done" && (
+      <Switch>
+        {(builds.BuildInfoError ||
+          builds.buildLogError ||
+          builds.buildListError ||
+          settings.settingsErorr) && <Redirect to="/page-not-found"></Redirect>}
+        {settings.status === 'done' && (
+          <Route
+            exact
+            path="/"
+            render={() =>
+              getter.repoName ? <BuildsHistoryPage /> : <StartScreenPage />
+            }
+          ></Route>
+        )}
+        <Route exact path="/"></Route>
+        <Route exact path="/settings" component={SettingsPage}></Route>
         <Route
           exact
-          path="/"
-          render={() =>
-            getter.repoName ? <BuildsHistoryPage /> : <StartScreenPage />
-          }
+          path="/build/:number"
+          component={BuildDetaildsPage}
         ></Route>
-      )}
-      <Route exact path="/settings" component={SettingsPage}></Route>
-      <Route exact path="/build/:number" component={BuildDetaildsPage}></Route>
-      <Route exact path="/page-not-found" component={PageNotFound}></Route>
+        <Route exact component={PageNotFound}></Route>
+      </Switch>
     </div>
   );
 };
