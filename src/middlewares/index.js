02b75aaf097ee53ed60d9cf3e02860c978f16a09
PavelIgnatev <palllkaignatev@gmail.com>
2021-07-12 19:34:57 +0300

    fix statics

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,6 @@
 const { json } = require('express');
 const disablePoweredBy = require('./disablePoweredBy');
 
-const cors = require('cors');
 const rateLimiter = require('express-rate-limit');
 const slowDown = require('express-slow-down');
 
@@ -9,12 +8,6 @@ module.exports = (app) => {
   app.use(json());
 
   app.use(disablePoweredBy);
-
-  app.use(
-    cors({
-      origin: ['http://localhost:3001', 'https://gitlubereact.web.app'],
-    })
-  );
 
   const limiter = rateLimiter({
     windowMs: 1 * 60 * 1000,
