58c119962724aa39985e12a484a04a2809a03de4
PavelIgnatev <palllkaignatev@gmail.com>
2021-07-14 21:16:38 +0300

    fix .hermione.conf.js

--- .hermione.conf.js.js
+++ .hermione.conf.js.js
@@ -1,6 +1,8 @@
 module.exports = {
   baseUrl: 'http://localhost:3000/',
+
   gridUrl: 'http://127.0.0.1:4444/wd/hub',
+
   browsers: {
     chrome: {
       desiredCapabilities: {
@@ -8,6 +10,7 @@ module.exports = {
       },
     },
   },
+
   sets: {
     desktop: {
       files: [
