1a9c3af3833d9cbc8ebe0c7e7b0f7d9cc643ab53
PavelIgnatev <palllkaignatev@gmail.com>
2021-07-17 17:44:58 +0300

    fix config

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,7 @@ const axiosAndKey = defaults.create({
   headers: { Authorization: `Bearer ${process.env.SWAGGER_KEY}` },
 });
 
-const PORT = 3000;
+const PORT: number = 3000;
 const axios = axiosAndKey;
 
 export { PORT, axios };
